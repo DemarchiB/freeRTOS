7f5561dc007334c351e91268f18dcb75c0c45123
bdemarchi <brunoluizdemarchi@outlook.com>
2023-01-10 14:08:12 -0300

    Criado novo projeto com os arquivos do FreeRTOS e corrigidos todos os erros de compilação.

---
+++ RTOS_workspace/FreeRTOS_Projects/Debug/ThirdParty/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,20 @@
+ThirdParty/portable/GCC/ARM_CM4F/port.o: \
+ ../ThirdParty/portable/GCC/ARM_CM4F/port.c \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/FreeRTOS.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOSConfig.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/projdefs.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/portable.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/deprecated_definitions.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/portable/GCC/ARM_CM4F/portmacro.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/mpu_wrappers.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/task.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/list.h
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/FreeRTOS.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOSConfig.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/projdefs.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/portable.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/deprecated_definitions.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/portable/GCC/ARM_CM4F/portmacro.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/mpu_wrappers.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/task.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/list.h:
---
+++ RTOS_workspace/FreeRTOS_Projects/Debug/ThirdParty/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+ThirdParty/portable/MemMang/heap_4.o: \
+ ../ThirdParty/portable/MemMang/heap_4.c \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/FreeRTOS.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOSConfig.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/projdefs.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/portable.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/deprecated_definitions.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/portable/GCC/ARM_CM4F/portmacro.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/mpu_wrappers.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/task.h \
+ E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/list.h
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/FreeRTOS.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOSConfig.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/projdefs.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/portable.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/deprecated_definitions.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/portable/GCC/ARM_CM4F/portmacro.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/mpu_wrappers.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/task.h:
+E:/freeRTOS-course/RTOS_workspace/FreeRTOS_Projects/ThirdParty/include/list.h:
